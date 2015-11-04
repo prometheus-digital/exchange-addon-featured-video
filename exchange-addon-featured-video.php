bf6d2f58fcc85870fe9ed8fe68dc3117dbf7d55c
Lew Ayotte <lew@ithemes.com>
2015-11-04 15:29:56 -0600

    Tagging 1.1.0

--- exchange-addon-featured-video.php
+++ exchange-addon-featured-video.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: iThemes Exchange - Featured Video Add-on
- * Version: 1.0.8
+ * Version: 1.1.0
  * Description: Adds the featured video to iThemes Exchange products.
  * Plugin URI: http://ithemes.com/exchange/featured-video/
  * Author: iThemes
